552cc5c172f8fef8db7f6b3e6dcc3a8520d5804e
Omer Ozceylan <omerozceylan40@gmail.com>
2022-10-13 11:10:30 +0300

    bazı ekstra butonlar..

--- js.js
+++ js.js
@@ -4,18 +4,27 @@ let baslikCounter = document.querySelector("#counter")
 
 let arttirButton = document.querySelector("#arttirButtonInHTML")
 let azaltButton = document.querySelector("#azaltButtonInHTML")
+let sifirlaButton = document.querySelector("#sifirlaButtonInHTML")
+let artiOnArttirButton = document.querySelector("#artiOnArttirButtonInHTML")
 
 arttirButton.style.backgroundColor = "white"
 azaltButton.style.backgroundColor = "white"
 
 arttirButton.addEventListener("click" , counterReact)
 azaltButton.addEventListener("click" , counterReact )
+sifirlaButton.addEventListener("click" , counterReact)
+artiOnArttirButton.addEventListener("click" , counterReact)
 
 function counterReact(){
     if(this.id == "arttirButtonInHTML"){
         counter += 1
-    }
-    else if(!counter == 0){
+    }else if(this.id == "artiOnArttirButtonInHTML"){
+        counter +=10
+
+    }else if(this.id == "sifirlaButtonInHTML"){
+        counter = 0
+        
+    }else if(!counter == 0){
         counter -= 1
     }
     baslikCounter.innerHTML = counter
